efcf0e924a01cdcd9d3cbdfbcd50d5c3091f4c34
fhpthh <huehongphan0807@gmail.com>
2025-12-05 08:09:20 +0000

    add

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://192.168.126.99:8888/api/v1/employees";
 
 class EmployeeService {
 
